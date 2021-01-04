a2ea70acdedfe316de9dd86ab69bd5b3c126a36e
danielablancom <blancodanielac@gmail.com>
2021-01-04 17:14:02 -0300

    Geolocation modal

---
+++ src/Components/Form/ThemeGeo.js
@@ -0,0 +1,14 @@
+import {createMuiTheme} from "@material-ui/core/styles"
+
+
+const themeGeo = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#5C3ECA',
+        },
+     
+    }
+
+})
+
+export default themeGeo; 
--- src/Pages/FormPage.js
+++ src/Pages/FormPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Geolocation from '../Components/Form/Geolocation'
 
 
 const FormPage = () => {
     return (
         <div>
             <h1>FOOOOOOOOOOOOOOOOOOOOOORM</h1>
+            <Geolocation />
         </div>
     )
 }
