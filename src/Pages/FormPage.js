e893c4c3c82b22d67683a55eba25da5b24f7a50c
Victoria <victoria.ancalaf@gmail.com>
2021-01-04 19:03:38 -0300

    gender component

--- src/Pages/FormPage.js
+++ src/Pages/FormPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import Geolocation from "../Components/Form/Geolocation";
 import ModalWelcome from "../Components/Form/modalWelcome";
 import Age from "../Components/Form/Age";
+import Gender from "../Components/Form/Gender";
 
 const FormPage = () => {
   return (
     <div>
       <ModalWelcome />
       <Age />
+      <Gender />
       <Geolocation />
     </div>
   );
