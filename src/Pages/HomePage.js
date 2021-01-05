99ff9b9ed53efa9775a291892dd46656fc359186
Ane Acevedo <ane.acevedo@gmail.com>
2021-01-05 11:02:18 -0300

    Dashboard listo

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Saldo from "../Components/Home/Saldo";
 import Carousel from "../Components/Home/carousell";
+import Dashboard from "../Components/Home/Dashboard";
 
 const HomePage = () => {
   return (
     <div>
       <Saldo />
       <Carousel />
+      <Dashboard />
     </div>
   );
 };
