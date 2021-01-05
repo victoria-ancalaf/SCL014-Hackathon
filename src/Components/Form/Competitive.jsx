79f1cc291ac9c468b3017b844b5de932698205bf
danielablancom <blancodanielac@gmail.com>
2021-01-04 22:32:49 -0300

    Competitive component Form

--- src/Components/Form/Competitive.jsx
+++ src/Components/Form/Competitive.jsx
@@ -1,17 +1,23 @@
 import React, { Fragment } from "react";
-import './Styles/Competitive.css'
+import "./Styles/Competitive.css";
 
 const Competitive = () => {
   return (
     <Fragment>
       <div className="container-competitive">
         <div className="box-textCompetitive">
-          <h1 className="text-Competitive">¿Te consideras competitiv@?</h1>
+          <h2 className="text-Competitive">¿Te consideras competitiv@?</h2>
         </div>
-        <div>
-          <button className="btn-Competitive">Si, mucho</button>
-          <button className="btn-Competitive"> No, para nada</button>
-          <button className="btn-Competitive">Lo justo y necesario</button>
+        <div className="box-btn">
+          <div>
+            <button className="btn-Competitive">Si, mucho</button>
+          </div>
+          <div>
+            <button className="btn-Competitive"> No, para nada</button>
+          </div>
+          <div>
+            <button className="btn-Competitive">Lo justo y necesario</button>
+          </div>
         </div>
       </div>
     </Fragment>
