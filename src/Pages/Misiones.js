5e53d17d33c33c8e0fef8295a7a8a46444dbbfc6
Victoria <victoria.ancalaf@gmail.com>
2021-01-05 17:16:03 -0300

    buttons missions

--- src/Pages/Misiones.js
+++ src/Pages/Misiones.js
@@ -1,44 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderMisiones from "../Components/Missions/HeaderMisiones";
 import Permanentes from "../Components/Missions/Permanentes";
-import Tab from 'react-bootstrap/Tab'
-import Nav from 'react-bootstrap/Nav'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Temporales from "../Components/Missions/Temporales";
 
 const Misiones = () => {
+  const [showPermanentes, setShowPermanentes] = useState(true);
+  const [showTemporales, setShowTemporales] = useState(false);
+
+  const handleClickPermanentes = () => {
+    setShowPermanentes(true);
+    setShowTemporales(false);
+  };
+
+  const handleClickTemporales = () => {
+    setShowTemporales(true);
+    setShowPermanentes(false);
+  };
+
   return (
     <div>
       <HeaderMisiones />
-      {/* <section className="buttons-saldo-container">
-        <button className="saldo-btn">Misiones Temporales</button>
-        <button className="saldo-btn">Misiones permanentes</button>
+      <section className="buttons-saldo-container">
+        <button onClick={handleClickTemporales} className="saldo-btn">
+          Misiones Temporales
+        </button>
+        <button onClick={handleClickPermanentes} className="saldo-btn">
+          Misiones permanentes
+        </button>
       </section>
-      <Permanentes /> */}
-     <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-  <Row>
-    <Col sm={3}>
-      <Nav variant="pills" className="flex-column">
-        <Nav.Item>
-          <Nav.Link eventKey="first">Tab 1</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="second">Tab 2</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Col>
-    <Col sm={9}>
-      <Tab.Content>
-        <Tab.Pane eventKey="first">
-          <h1>olo</h1>
-        </Tab.Pane>
-        <Tab.Pane eventKey="second">
-        <h1>chau</h1>
-        </Tab.Pane>
-      </Tab.Content>
-    </Col>
-  </Row>
-</Tab.Container>
+      <Permanentes visible={showPermanentes} />
+      <Temporales visible={showTemporales} />
     </div>
   );
 };
