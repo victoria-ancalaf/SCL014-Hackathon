c8e1fd0df0851fa25b28216cfb67bac83419788a
Victoria <victoria.ancalaf@gmail.com>
2021-01-05 14:34:33 -0300

    misiones component

---
+++ src/Components/Missions/HeaderMisiones.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Styles/HeaderMisiones'
+
+const HeaderMisiones = () => {
+    return (
+        <div className="header-mision-cont">
+            <div className="mision-header-1">
+                <h2 className="mision-text">{"<"}</h2>
+            </div>
+            <div className="mision-header-2">
+                <h2 className="mision-text">Misiones</h2>
+            </div>
+            <div className="mision-header-3"></div>
+        </div>
+    )
+}
+
+export default HeaderMisiones
