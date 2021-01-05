6c5ee0cb4a3cbf9db909dd9b12ec69dce32a1a00
Francisco Orellana <fraciscoorellana@MacBook-Pro-de-Fracisco.local>
2021-01-05 09:07:37 -0300

    Revision cambios

--- src/Components/Form/Dashboard.jsx
+++ src/Components/Form/Dashboard.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const dashboard = () => {
     return(
         <div>
-
+           <div>
+               <image></image>
+               <h1>Equipo MACH</h1>
+               <p>
+                   Juana, acabas de realizar una transferencia de $5.000 a tu cuenta MACH.
+                </p>
+           </div>
+           <div>
+               <image></image>
+               <h1>Equipo MACH</h1>
+               <p>Juana, acabas de realizar un pago de $20.000 a la cuenta...</p>
+           </div>
         </div>
     )
 }
