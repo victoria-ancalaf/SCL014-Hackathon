0c5229d909e349f08d39ea243d9c3e4b843bb9df
Paula Frías <paula.frias.navarro@gmail.com>
2021-01-05 15:06:04 -0300

    agregar imgs, empezar modal para categorias

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import MisionPoints from '../Components/Categories/MisionPoint';
 import InfoPointDiscount from '../Components/Categories/infoPointDiscount';
+import NewMacher from '../Components/Categories/NewMacher';
+
 
 const Categories = () => {
     return (
         <div>
-           <MisionPoints/>
-           <InfoPointDiscount/>
+           {/* <MisionPoints/> */}
+           {/* <InfoPointDiscount/> */}
+           <NewMacher/>
         </div>
     )
 }
