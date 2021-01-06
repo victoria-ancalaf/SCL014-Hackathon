a389ef912666a99289bedb621ce91df9b0860e28
Paula Frías <paula.frias.navarro@gmail.com>
2021-01-05 21:32:32 -0300

    categorias por niveles terminado

---
+++ src/Components/Categories/TrueMacher.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+import woman from "../../Img/runing.svg";
+import lock from "../../Img/lock.svg";
+import "./Styles/NewMacher.css";
+
+const TrueMacher = () => {
+  return (
+    <Fragment>
+      <div className="headerLevels">
+        <p> {"<"} </p>
+        <p>Nivel 2</p>
+        <p> </p>
+      </div>
+      <div className="containerMasterLevels">
+        <div className="containerCardLevel">
+          <div className="headCardLevel2">
+            <div className="containerTitle">
+              <p>True Macher</p>
+              <div className="containerImgLevelsWoman">
+                <img className="imgLevel" src={woman} alt="woman" />
+              </div>
+              <div className="containerImgLock">
+                <img className="imgLevel" src={lock} alt="lock" />
+              </div>
+            </div>
+          </div>
+          <div className="bodyCardLevel">
+            <p>
+            Perteneces a esta categoría si cuentas con entre 100 y 199 MACH Points, lo que te permite acceder a:
+            </p>
+            <ul>
+              <li className='list2'>Por cada $5.000 pesos gastados, te damos 5 puntos.</li>
+              <li className='list2'>Hasta 35% de descuento en productos seleccionados.</li>
+              <li className='list2'>1 Caja sorpresa de regalo anual</li>
+              <li className='list2'>Devoluciones de dinero, por compras específicas.</li>
+              <li className='list2'>Descuentos personalizados, en base a tus preferencias.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default TrueMacher;
--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -2,14 +2,16 @@ import React from 'react'
 import MisionPoints from '../Components/Categories/MisionPoint';
 import InfoPointDiscount from '../Components/Categories/infoPointDiscount';
 import NewMacher from '../Components/Categories/NewMacher';
-
-
+import TrueMacher from '../Components/Categories/TrueMacher';
+import MachLover from '../Components/Categories/MachLover';
 const Categories = () => {
     return (
         <div>
            {/* <MisionPoints/> */}
            {/* <InfoPointDiscount/> */}
-           <NewMacher/>
+           {/* <NewMacher/> */}
+           {/* <TrueMacher/> */}
+           <MachLover/>
         </div>
     )
 }
