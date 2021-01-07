18b0f7287f479cb8dca93f706e1270616ce2ddd4
Paula Frías <paula.frias.navarro@gmail.com>
2021-01-07 10:57:39 -0300

    icon machelo terminado junto con su funcionalidad

--- src/Components/Home/iconMachelo.jsx
+++ src/Components/Home/iconMachelo.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Styles/iconMachelo.css';
+import React, { useState, Fragment } from "react";
+import ModalMachelo from "./modalMachelo";
+import ola from '../../Img/iconMachelo.png';
+import "./Styles/iconMachelo.css";
 
 const IconMachelo = () => {
-    return (
-        <div>
-            icon
-        </div>
-    );
-}
+  let [modalOn, setModalOn] = useState(false);
+  return (
+    <Fragment>
+      <div className="containerIconMachelo" modalOn={modalOn} onClick={() => setModalOn(!modalOn) }>
+        <img className="iconMachelito" src={ola} alt="icon" />
+      </div>
+      <ModalMachelo modalOn={modalOn} setModalOn={setModalOn}/>
+    </Fragment>
+  );
+};
 
 export default IconMachelo;
--- src/Components/Home/modalMachelo.jsx
+++ src/Components/Home/modalMachelo.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import './Styles/modalMachelo.css';
 
-const ModalMachelo = () => {
+const ModalMachelo = ({modalOn, setModalOn}) => {
+    const classModal = modalOn === true ? 'modalVisible' : 'modalInvisible'
     return(
-        <div>
+        <div className={classModal} modalOn={modalOn} onClick={() => setModalOn(!modalOn)}>
             Mo0dalmacheo
         </div>
     );
